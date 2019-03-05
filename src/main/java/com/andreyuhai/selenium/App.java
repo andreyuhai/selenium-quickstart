654eb62bd2e60508391aa240ee9059a8511a5128
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-03-05 11:52:44 +0100

    Blank main

--- src/main/java/com/andreyuhai/selenium/App.java
+++ src/main/java/com/andreyuhai/selenium/App.java
@@ -1,13 +1,11 @@
 package com.andreyuhai.selenium;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
+
+
 }
