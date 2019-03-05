0622189544d7fd223be0ce10e5df7b784cb320d1
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-03-05 11:52:30 +0100

    Base BotBehaviour for creating Selenium apps

---
+++ src/main/java/com/andreyuhai/selenium/bot/BaseBotBehaviour.java
@@ -0,0 +1,53 @@
+package com.andreyuhai.selenium.bot;
+
+import com.sun.xml.internal.rngom.parse.host.Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class BaseBotBehaviour implements BotBehaviour {
+
+    WebDriver driver;
+
+    public BaseBotBehaviour(int browser_type, boolean headless, boolean incognito){
+        initializeDriver(browser_type, headless, incognito);
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+
+    private void initializeDriver(int browser_type, boolean headless, boolean incognito) {
+        if (browser_type == Browser.FIREFOX ) {
+            driver = createFirefoxDriver(headless, incognito);
+        } else {
+            driver = createChromeDriver(headless, incognito);
+        }
+    }
+
+    // Creates a Chrome driver and returns it.
+    private ChromeDriver createChromeDriver(boolean headless, boolean incognito) {
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(headless);
+        if (incognito) {
+            options.addArguments("incognito");
+        }
+        return new ChromeDriver(options);
+    }
+
+    // Creates a Firefox driver and returns it.
+    private FirefoxDriver createFirefoxDriver(boolean headless, boolean incognito) {
+        FirefoxOptions options = new FirefoxOptions();
+        if (headless) {
+            options.addArguments("--headless");
+        }
+        if (incognito) {
+            options.addArguments("-private");
+        }
+        return new FirefoxDriver(options);
+    }
+}
