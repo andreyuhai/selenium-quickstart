df1fec987ec8dc1e7a4edc7ae31997462f7934d9
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-03-05 11:50:19 +0100

    Changed to be simple as possible so you can extend this class

--- src/main/java/com/andreyuhai/selenium/bot/SeleniumBot.java
+++ src/main/java/com/andreyuhai/selenium/bot/SeleniumBot.java
@@ -1,51 +1,14 @@
 package com.andreyuhai.selenium.bot;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+public class SeleniumBot {
 
-public class SeleniumBot implements BotBehaviour {
+    BotBehaviour botBehaviour;
 
-    WebDriver driver;
-
-    public SeleniumBot(int browser_type, boolean headless, boolean incognito){
-        initializeDriver(browser_type, headless, incognito);
+    public void setBotBehaviour(BotBehaviour botBehaviour) {
+        this.botBehaviour = botBehaviour;
     }
 
-    @Override
     public void run() {
-
-    }
-
-    private void initializeDriver(int browser_type, boolean headless, boolean incognito) {
-        if (browser_type == Browser.FIREFOX ) {
-            driver = createFirefoxDriver(headless, incognito);
-        } else {
-            driver = createChromeDriver(headless, incognito);
-        }
-    }
-
-    // Creates a Chrome driver and returns it.
-    private ChromeDriver createChromeDriver(boolean headless, boolean incognito) {
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(headless);
-        if (incognito) {
-            options.addArguments("incognito");
-        }
-        return new ChromeDriver(options);
-    }
-
-    // Creates a Firefox driver and returns it.
-    private FirefoxDriver createFirefoxDriver(boolean headless, boolean incognito) {
-        FirefoxOptions options = new FirefoxOptions();
-        if (headless) {
-            options.addArguments("--headless");
-        }
-        if (incognito) {
-            options.addArguments("-private");
-        }
-        return new FirefoxDriver(options);
+        botBehaviour.run();
     }
 }
